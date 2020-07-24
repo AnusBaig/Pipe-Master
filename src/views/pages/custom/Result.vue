6f961fc3c11db4dff0eaeaa9eb7f47f7e60d5f48
AnusBaig <48921961+AnusBaig@users.noreply.github.com>
2020-07-24 07:05:50 +0500

    Add results page

--- src/views/pages/custom/Result.vue
+++ src/views/pages/custom/Result.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
-        <div class="flex justify-around text-center">
-        <vs-button size="large" to="/requirement">Back</vs-button>
+        <div class="flex justify-end">
+            <vs-button size="small" to="">Print</vs-button>
+        </div>
+
+        <Grid></Grid>
+
+        <div class="flex justify-around text-center mt-5">
+            <vs-button size="large" to="/requirement">Back</vs-button>
         </div>
     </div>
 </template>
 
 <script>
+import Grid from '../../ui-elements/grid/vuesax/GridDesignConcept'
+
+export default{
+    components:{
+        Grid
+    }
+}
 </script>
 
 <style lang="sass" scoped>
