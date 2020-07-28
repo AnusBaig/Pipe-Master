7eeddbbb63d4f4a54c6132069adf94e673374351
AnusBaig <48921961+AnusBaig@users.noreply.github.com>
2020-07-29 03:00:06 +0500

    Build for prod

--- serviceWorker.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('install',e=>console.log('[Service Worker] Installing ... ', e))
-
-self.addEventListener('activate',e=>console.log('[Service Worker] Activating ... ', e))
-
-self.addEventListener('fetch',() => console.log('[Service Worker] Fetching ... '))
